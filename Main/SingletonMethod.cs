a946f42c6993ad77b9c61440eba081af3d2c77e2
riekure <s2darl0uvgia24@gmail.com>
2020-04-04 21:46:07 +0900

    add Singleton

---
+++ Main/SingletonMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Singleton
+{
+    public class SingletonMethod
+    {
+        public static void SingletonMain()
+        {
+            Console.WriteLine("Start.");
+            Singleton obj1 = Singleton.GetInstance();
+            Singleton obj2 = Singleton.GetInstance();
+            if (obj1 == obj2)
+            {
+                Console.WriteLine("obj1とobj2は同じインスタンスです。");
+            }
+            Console.WriteLine("End.");
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Singleton
+{
+    public class Singleton
+    {
+        static readonly Singleton singleton = new Singleton();
+        private Singleton()
+        {
+            Console.WriteLine("インスタンスを生成しました。");
+        }
+
+        public static Singleton GetInstance()
+        {
+            return singleton;
+        }
+    }
+}
